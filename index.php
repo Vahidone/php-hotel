5bfa37c651062a3d7a8907495b3ad2923683b593
Vahidone <v.ban.roma@gmail.com>
2023-10-26 17:29:41 +0200

    perf: improve text writing

--- index.php
+++ index.php
@@ -44,11 +44,12 @@ $hotels = [
 ?>
 
 <?php
+
 $filteredHotels = $hotels;
 
 if (isset($_GET['parking']) && $_GET['parking'] == 1) {
     $filteredHotels = array_filter($filteredHotels, function ($hotel) {
-        return $hotel['parking'] == true;
+      return $hotel['parking'] == true;
     });
 }
 
@@ -66,55 +67,60 @@ if (empty($_GET)) {
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-   <!-- link BOOTSTRAP  -->
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Hotel</title>
-</head>
-<body>
-<div class="container mt-5 p-5">
-    <form action="" method="GET">
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="parkingCheckbox" name="parking" value="1">
-            <label class="form-check-label" for="parkingCheckbox">Mostra solo hotel con il parcheggio</label>
-        </div>
-        <div class="mb-3">
-            <label for="voteInput" class="form-label">Voto minimo:</label>
-            <input type="number" class="form-control" id="voteInput" name="vote" min="1" max="5">
-        </div>
-        <button type="submit" class="btn btn-success">Filtra</button>
-    </form>
-</div>
+  <head>
+    <!-- link BOOTSTRAP  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+  </head>
 
-<div class="container border border-1 ">
-    <h2 class="text-center fw-bold text-primary">Lista degli hotel</h2>
-    <table class="table text-center">
+
+  <body>
+    <!-- la sezione relativa al filter degli hotel a base del parcheggio e del voto  -->
+    <div class="container mt-5 p-5">
+        <form action="" method="GET">
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="parkingCheckbox" name="parking" value="1">
+              <label class="form-check-label" for="parkingCheckbox">Mostra solo hotel con il parcheggio</label>
+            </div>
+            <div class="mb-3">
+              <label for="voteInput" class="form-label">Voto minimo:</label>
+              <input type="number" class="form-control" id="voteInput" name="vote" min="1" max="5">
+            </div>
+            <button type="submit" class="btn btn-success">Filtra</button>
+        </form>
+    </div>
+
+
+    <!-- tabella con i dati degli hotel  -->
+    <div class="container border border-1 ">
+      <h2 class="text-center fw-bold text-primary">Lista degli hotel</h2>
+      <table class="table text-center">
         <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Descrizione</th>
-                <th>Parcheggio</th>
-                <th>Voto</th>
-                <th>Distanza dal centro</th>
-            </tr>
+          <tr>
+            <th>Nome</th>
+            <th>Descrizione</th>
+            <th>Parcheggio</th>
+            <th>Voto</th>
+            <th>Distanza dal centro</th>
+          </tr>
         </thead>
         <tbody>
-            <?php foreach ($filteredHotels as $hotel) { ?>
-                <tr>
-                    <td><?php echo $hotel['name']; ?></td>
-                    <td><?php echo $hotel['description']; ?></td>
-                    <td><?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></td>
-                    <td><?php echo $hotel['vote']; ?></td>
-                    <td><?php echo $hotel['distance_to_center']; ?></td>
-                </tr>
-            <?php } ?>
+          <?php foreach ($filteredHotels as $hotel): ?>
+            <tr>
+              <td><?php echo $hotel['name']; ?></td>
+              <td><?php echo $hotel['description']; ?></td>
+              <td><?php echo $hotel['parking'] ? 'SI' : 'NO'; ?></td>
+              <td><?php echo $hotel['vote']; ?></td>
+              <td><?php echo $hotel['distance_to_center']; ?></td>
+            </tr>
+          <?php endforeach; ?>
         </tbody>
-    </table>
-</div>
+      </table>
+    </div>
 
-</body>
+  </body>
 </html>
